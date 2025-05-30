79cce150677d075e80615cb294fa0467a829de9e
vtune <vvlukanin13@mail.ru>
2025-05-30 20:23:44 +0000

    change style

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,10 @@
 
 // Main function that asks for user name
 // and prints greeting message
-int main() {
-  std::string name;  // Variable to store user name
+int
+main()
+{
+  std::string name; // Variable to store user name
   std::cout << "Enter your name: ";
   std::cin >> name;
   std::cout << "Hello world from " << name << std::endl;
